5907c98bbcd9767bf55bf07e1c60bf3863a6821e
Almamamia <itsalmakim@gmail.com>
2023-04-22 13:47:18 -0400

    stage03_wip

--- scripts/index.js
+++ scripts/index.js
@@ -28,11 +28,30 @@ let initialCards = [
 const profileEditBtn = document.querySelector(".profile__edit-button");
 const porfileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseBtn = document.querySelector(".modal__close");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = document.querySelector("#form-input-name");
+const profileDescriptionInput = document.querySelector(
+  "#form-input-description"
+);
+const profileEditForm = porfileEditModal.querySelector(".modal__form");
+
+function closeForm() {
+  porfileEditModal.classList.remove("modal_opened");
+}
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeForm();
+}
 
 profileEditBtn.addEventListener("click", () => {
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   porfileEditModal.classList.add("modal_opened");
 });
 
-profileCloseBtn.addEventListener("click", () => {
-  porfileEditModal.classList.remove("modal_opened");
-});
+profileCloseBtn.addEventListener("click", closeForm);
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
