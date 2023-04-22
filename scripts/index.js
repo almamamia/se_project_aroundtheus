12c4a3ae94c16086316e14b06488e3832f8ff810
Almamamia <itsalmakim@gmail.com>
2023-04-22 15:34:08 -0400

    Project04_submission

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,7 @@ let initialCards = [
   },
 ];
 
+//elements
 const profileEditBtn = document.querySelector(".profile__edit-button");
 const porfileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseBtn = document.querySelector(".modal__close");
@@ -35,16 +36,32 @@ const profileDescriptionInput = document.querySelector(
   "#form-input-description"
 );
 const profileEditForm = porfileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector(".cards__list");
 
+//functions
 function closeForm() {
   porfileEditModal.classList.remove("modal_opened");
 }
 
+//event handlers
 function handleProfileEditSubmit(e) {
   e.preventDefault();
   profileName.textContent = profileNameInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
   closeForm();
+}
+
+//event listeners
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
+  return cardElement;
 }
 
 profileEditBtn.addEventListener("click", () => {
@@ -55,3 +72,8 @@ profileEditBtn.addEventListener("click", () => {
 
 profileCloseBtn.addEventListener("click", closeForm);
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});
